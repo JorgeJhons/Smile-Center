119c2c434fe85caa590f26c626aec001026ac5f2
Jorge Jhons <jfernandezcolli@gmail.com>
2015-07-31 17:49:31 -0500

    mi primer commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset ="utf-8">
+	<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<title>Mi encabezado</title>
+</head>
+<body>
+<div  class="col-md-12 center-block float text-center espacioarriva" id="principal" >
+	<img src="2.png">
+	<h1 class="roboto verde">Smile Center QR</h1>
+	<h2>Clinicas Dentales Quintana Roo</h2>
+	<nav>
+
+		<a class="espacioderecha" href="#">Menu</a>
+		<a class="espacioderecha" href="#">Contacto</a>
+		<a class="espacioderecha" href="#">Hacerca de nosotros</a>
+
+	</nav>
+</div>
+</body>
+</html>
